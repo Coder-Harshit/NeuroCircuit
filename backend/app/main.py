2afbfe3757c0a83932dea7bd548d9110935be994
Harshit Vijay <93333205+Coder-Harshit@users.noreply.github.com>
2025-09-28 20:27:17 +0530

    [+] DependencyList

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,8 @@
+from typing import Dict, Set, Tuple, Union
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from classes import GraphPayload
+import graphlib
 
 app = FastAPI()
 
@@ -22,13 +24,43 @@ def read_root():
     return {"message": "Hello from the AI Graph Executor Backend!"}
 
 @app.post('/execute')
-def execute_graph(graph: GraphPayload):
-    print("Received Graph Data:")
-    print(graph.model_dump_json(indent=2))
-    return {
-        "status": "success",
-        "message": "Graph received successfully!"
-    }
+def execute_graph(graph: GraphPayload) -> Dict[str, Union[str, Tuple[str, ...] ]]:
+    print(graph.edges)
+    # We would need to perform topological sort as the nodes could be in random order, i.e. to make sure dependencies are finished first and then only main task is executed
+
+    # dep_list => DEPENDENCY LIST (opposite of ADJ. LIST)
+    dep_list: Dict[str, Set[str]] = {node.id: set() for node in graph.nodes}
+    # generated the dep_list
+
+    for edg in graph.edges:
+        # going from tgt to source becuase we actually want to generate the dependency list
+        dep_list[edg.target].add(edg.source)
+    
+    print(f"Constructed Adjacency List: {dep_list}")
+
+
+    # PERFORM topological sort
+    try:
+        ts: graphlib.TopologicalSorter[str] = graphlib.TopologicalSorter(dep_list)
+        
+        # Tuple used for immutability &
+        # elipse used to denote tuple of arbitary length
+        exec_order: Tuple[str, ...] = tuple(ts.static_order()) 
+        print(f"Correct execution order: {exec_order}")
+        
+        for node_id in exec_order:
+            print(f"Executing node: {node_id}")
+
+        return {
+            "status": "success",
+            "message": "Graph sorted successfully!",
+            "execution_order": exec_order
+        }
+    except graphlib.CycleError:
+        return {
+            "status": "error",
+            "message": "Graph contains a cycle and cannot be executed."
+        }
 
 if __name__ == "__main__":
     pass
